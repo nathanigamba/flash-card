6ad788e9dda8c0009e2664b57ee02fae72fee036
nathanigamba <143838103+nathanigamba@users.noreply.github.com>
2024-10-01 16:16:49 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Corpo Humano',
+    'Qual o maior órgão do corpo humano?',
+    'A pele'
+)
+
+criaCartao(
+    'Corpo humano',
+    'Quantos ossos possui o coorpo humano?',
+    'Possui 206 ossos'
+)
+
+criaCartao(
+    'Corpo humano',
+    'Onde são produzidos os glóbulos vermelhos?',
+    'Na medula óssea'
+)
+
+criaCartao(
+    'Corpo humano',
+    'O que conecta múscolos do corpo?',
+    'Os tendões'
+)
